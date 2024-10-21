c27cab3b7b2fd02b20f66d4c2a25bd815f22bde4
Christof Becu <christof.becu@gmail.com>
2024-10-21 10:07:38 +0200

    print command line arguments to stdout

--- ccwc/c++/src/main.cpp
+++ ccwc/c++/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <algorithm>
+#include <iterator>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    std::copy(argv, argv + argc, std::ostream_iterator<char *>(std::cout, "\n"));
 }
