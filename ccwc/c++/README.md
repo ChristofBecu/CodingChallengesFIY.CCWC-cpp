a9aa73f129c3851a860c1a91af7a08b60892d9ac
Christof Becu <christof.becu@gmail.com>
2024-10-21 10:48:24 +0200

    count bytes in a file

--- ccwc/c++/README.md
+++ ccwc/c++/README.md
@@ -3,3 +3,29 @@
 ## Build executable
 
 - make -f Makefile
+
+## Usage
+- ccwc -c data/test.txt
+
+## Things learned
+
+### loop over arguments
+```cpp
+std::copy(argv, argv + argc, std::ostream_iterator<char *>(std::cout, "\n"));
+```
+### open / close file
+```cpp
+std::ifstream input_file(file_name, std::ios::in);
+if (!input_file.is_open())
+{
+    std::cout << "File not found : " << file_name << std::endl;
+    return 4;
+}
+input_file.close();
+```
+
+### count bytes in file
+```cpp
+input_file.seekg(0, std::ios::end);
+std::cout << "File size: " << input_file.tellg() << " bytes" << std::endl;
+```
--- ccwc/c++/src/main.cpp
+++ ccwc/c++/src/main.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+#include <string.h>
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
-    std::copy(argv, argv + argc, std::ostream_iterator<char *>(std::cout, "\n"));
+    #ifdef DEBUG
+        std::cout << "Debug mode" << std::endl;
+        std::copy(argv, argv + argc, std::ostream_iterator<char *>(std::cout, "\n"));
+    #endif
+
+
+    char *file_name = nullptr;
+    bool count_bytes = false;
+
+    if (argv[1] == nullptr)
+    {
+        std::cout << "No arguments passed" << std::endl;
+        return 1;
+    }
+
+    if (strcmp(argv[1], "-c") == 0)
+    {
+        count_bytes = true;
+    }
+
+    if (argv[2] == nullptr)
+    {
+        std::cout << "No file name passed" << std::endl;
+        return 3;
+    }
+
+    file_name = argv[2];
+    std::ifstream input_file(file_name, std::ios::in);
+    if (!input_file.is_open())
+    {
+        std::cout << "File not found : " << file_name << std::endl;
+        return 4;
+    }
+
+    if (count_bytes)
+    {
+        input_file.seekg(0, std::ios::end);
+        std::cout << input_file.tellg() << " " << file_name << std::endl;
+    }
+
+    input_file.close();
+
+    
+    
+
+
+
+
+
+
+
+   
+    return 0;
 }
